9d94eeb14bbce5e8b60189ac68e94dfbf5798ff0
Nell Hardcastle <nell@squishymedia.com>
2017-04-16 16:20:38 -0700

    Support specifying the snapshot to process.

--- run-bids-app.sh
+++ run-bids-app.sh
@@ -2,6 +2,7 @@
 # Environment variables:
 # BIDS_CONTAINER
 # BIDS_DIR_BUCKET
+# BIDS_SNAPSHOT_ID
 # OUTPUT_DIR_BUCKET
 # PARTICIPANT_FLAG
 # PARTICIPANT_LABEL
@@ -14,14 +15,14 @@ sleep 5
 
 if [ -z "$PARTICIPANT_FLAG" ]; then
     docker run -i --rm \
-	   -v /bids_dataset:/bids_dataset:ro \
-	   -v /outputs:/outputs \
+	   -v /bids_dataset/$BIDS_SNAPSHOT_ID:/bids_dataset:ro \
+	   -v /outputs/$BIDS_SNAPSHOT_ID:/outputs \
 	   $BIDS_CONTAINER \
-	   /bids_dataset /outputs participant --participant_label $PARTICIPANT_LABEL
+	   /bids_dataset /outputs group
 else
     docker run -i --rm \
-	   -v /bids_dataset:/bids_dataset:ro \
-	   -v /outputs:/outputs \
+	   -v /bids_dataset/$SNAPSHOT_ID:/bids_dataset:ro \
+	   -v /outputs/$SNAPSHOT_ID:/outputs \
 	   $BIDS_CONTAINER \
-	   /bids_dataset /outputs
+	   /bids_dataset /outputs participant --participant_label $PARTICIPANT_LABEL
 fi
