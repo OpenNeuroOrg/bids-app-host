1f759edec78fe028bf204db663251d9c8fe9bfae
Nell Hardcastle <nell@squishymedia.com>
2017-04-26 17:29:02 -0700

    Rename all env vars to BIDS_ prefix and pass extra parameters through env.

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 This container is meant to encapsulate [BIDS App containers](http://bids-apps.neuroimaging.io/) for execution with AWS roles and S3 inputs/outputs.
 
 ## Environment variables/configuration:
-* ANALYSIS_ID: A unique key for a combination of dataset and parameters
+* BIDS_ANALYSIS_ID: A unique key for a combination of dataset and parameters
 * BIDS_CONTAINER: path:tag for BIDS app container
-* BIDS_DIR_BUCKET: S3 Bucket containing BIDS directories
-* OUTPUT_DIR_BUCKET: Writable S3 Bucket for output
+* BIDS_DATASET_BUCKET: S3 Bucket containing BIDS directories
+* BIDS_OUTPUT_BUCKET: Writable S3 Bucket for output
 * BIDS_SNAPSHOT_ID: The key to reference which BIDS directory
-* BIDS_GROUP: Select for participant, group, etc
+* BIDS_ANALYSIS_LEVEL: Select for participant, group, etc
+* BIDS_ARGUMENTS: Optionally any additional parameters required
 
 ## Limitations
 Currently this only functions when running in an EC2 environment with IAM Roles assigned with access to the appropriate S3 buckets or with s3fs credentials injected into the container.
--- run-bids-app.sh
+++ run-bids-app.sh
@@ -8,23 +8,23 @@ set -eo pipefail
 
 if [ -z "$BIDS_CONTAINER" ]; then
     echo "Error: Missing env variable BIDS_CONTAINER." && exit 1
-elif [ -z "$BIDS_DIR_BUCKET" ]; then
-    echo "Error: Missing env variable BIDS_DIR_BUCKET." && exit 1
-elif [ -z "$OUTPUT_DIR_BUCKET" ]; then
-    echo "Error: Missing env variable OUTPUT_DIR_BUCKET." && exit 1
+elif [ -z "$BIDS_DATASET_BUCKET" ]; then
+    echo "Error: Missing env variable BIDS_DATASET_BUCKET." && exit 1
+elif [ -z "$BIDS_OUTPUT_BUCKET" ]; then
+    echo "Error: Missing env variable BIDS_OUTPUT_BUCKET." && exit 1
 elif [ -z "$BIDS_SNAPSHOT_ID" ]; then
     echo "Error: Missing env variable BIDS_SNAPSHOT_ID." && exit 1
-elif [ -z "$BIDS_GROUP" ]; then
-    echo "Error: Missing env variable BIDS_GROUP." && exit 1
-elif [ -z "$ANALYSIS_ID" ]; then
-    echo "Error: Missing env variable ANALYSIS_ID." && exit 1
+elif [ -z "$BIDS_ANALYSIS_ID" ]; then
+    echo "Error: Missing env variable BIDS_ANALYSIS_ID." && exit 1
+elif [ -z "$BIDS_ANALYSIS_LEVEL" ]; then
+    echo "Error: Missing env variable BIDS_ANALYSIS_LEVEL." && exit 1
 fi
 
 mkdir -p /tmp/bids_dataset
 mkdir -p /tmp/outputs
 
-s3fs -o "use_cache=/tmp/bids_dataset" -o "allow_other" -o "iam_role=auto" $BIDS_DIR_BUCKET /bids_dataset
-s3fs -o "use_cache=/tmp/outputs" -o "allow_other" -o "iam_role=auto" $OUTPUT_DIR_BUCKET /outputs
+s3fs -o "use_cache=/tmp/bids_dataset" -o "allow_other" -o "iam_role=auto" "$BIDS_DIR_BUCKET" /bids_dataset
+s3fs -o "use_cache=/tmp/outputs" -o "allow_other" -o "iam_role=auto" "$OUTPUT_DIR_BUCKET" /outputs
 
 # Make sure we've given time for Docker to start
 until [ -S /var/run/docker.sock ]; do
@@ -32,8 +32,8 @@ until [ -S /var/run/docker.sock ]; do
 done
 
 docker run -i --rm \
-   -v /bids_dataset/$BIDS_SNAPSHOT_ID:/bids_dataset:ro \
-   -v /outputs/$BIDS_SNAPSHOT_ID/$ANALYSIS_ID:/outputs \
-   $BIDS_CONTAINER \
-   /bids_dataset /outputs $BIDS_GROUP \
-   "$@"
+   -v /bids_dataset/"$BIDS_SNAPSHOT_ID":/bids_dataset:ro \
+   -v /outputs/"$BIDS_SNAPSHOT_ID"/"$BIDS_ANALYSIS_ID":/outputs \
+   "$BIDS_CONTAINER" \
+   /bids_dataset /outputs "$BIDS_ANALYSIS_LEVEL" \
+   "$BIDS_ARGUMENTS"
