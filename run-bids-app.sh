de10ac13b9caaa99c64b85b1ade9b4c108d4462f
Nell Hardcastle <nell@squishymedia.com>
2017-04-27 20:56:22 -0700

    Add a DEBUG var to skip s3fs mounting (for testing outside of AWS)

--- run-bids-app.sh
+++ run-bids-app.sh
@@ -6,9 +6,9 @@ set -eo pipefail
 
 if [ -z "$BIDS_CONTAINER" ]; then
     echo "Error: Missing env variable BIDS_CONTAINER." && exit 1
-elif [ -z "$BIDS_DATASET_BUCKET" ]; then
+elif [ -z "$BIDS_DATASET_BUCKET" ] && [ -z "$DEBUG" ]; then
     echo "Error: Missing env variable BIDS_DATASET_BUCKET." && exit 1
-elif [ -z "$BIDS_OUTPUT_BUCKET" ]; then
+elif [ -z "$BIDS_OUTPUT_BUCKET" ] && [ -z "$DEBUG" ]; then
     echo "Error: Missing env variable BIDS_OUTPUT_BUCKET." && exit 1
 elif [ -z "$BIDS_SNAPSHOT_ID" ]; then
     echo "Error: Missing env variable BIDS_SNAPSHOT_ID." && exit 1
@@ -18,11 +18,14 @@ elif [ -z "$BIDS_ANALYSIS_LEVEL" ]; then
     echo "Error: Missing env variable BIDS_ANALYSIS_LEVEL." && exit 1
 fi
 
+# Disable s3fs for debugging since it relies on EC2 roles
+if [ -z "$DEBUG" ]; then
 mkdir -p /tmp/bids_dataset
 mkdir -p /tmp/outputs
 
 s3fs -o "use_cache=/tmp/bids_dataset" -o "allow_other" -o "iam_role=auto" "$BIDS_DATASET_BUCKET" /bids_dataset
 s3fs -o "use_cache=/tmp/outputs" -o "allow_other" -o "iam_role=auto" "$BIDS_OUTPUT_BUCKET" /outputs
+fi
 
 # Make sure we've given time for Docker to start
 until docker ps; do
