5faf5c4727267fc7101101a6a9db71f3650e2564
Nell Hardcastle <nell@squishymedia.com>
2017-05-19 01:12:19 -0700

    Test for docker prior to making any docker calls.

--- run-bids-app.sh
+++ run-bids-app.sh
@@ -27,6 +27,20 @@ elif [ -z "$BIDS_ANALYSIS_LEVEL" ]; then
     echo "Error: Missing env variable BIDS_ANALYSIS_LEVEL." && exit 1
 fi
 
+# Make sure the host docker instance is running
+set +e # Disable -e because we expect docker ps to sometimes fail
+ATTEMPTS=1
+until docker ps &> /dev/null || [ $ATTEMPTS -eq 13 ]; do
+    sleep 5
+    ((ATTEMPTS++))
+done
+set -e
+
+if [ $ATTEMPTS -eq 13 ]; then
+    echo "Failed to find Docker service before timeout"
+    exit 1
+fi
+
 AWS_CLI_CONTAINER=infrastructureascode/aws-cli:1.11.89
 pull_and_prune "$AWS_CLI_CONTAINER"
 # Pull once, if pull fails, try to prune
@@ -53,20 +67,6 @@ docker run --rm -d --name "$AWS_BATCH_JOB_ID"-lock -v "$BIDS_SNAPSHOT_ID":/snaps
 # Sync those volumes
 docker run --rm -v "$BIDS_SNAPSHOT_ID":/snapshot $AWS_CLI_CONTAINER aws s3 sync s3://"$BIDS_DATASET_BUCKET"/"$BIDS_SNAPSHOT_ID" /snapshot
 docker run --rm -v "$BIDS_ANALYSIS_ID":/output $AWS_CLI_CONTAINER aws s3 sync s3://"$BIDS_OUTPUT_BUCKET"/"$BIDS_SNAPSHOT_ID"/"$BIDS_ANALYSIS_ID" /output
-
-# Make sure the host docker instance is running
-set +e # Disable -e because we expect docker ps to sometimes fail
-ATTEMPTS=1
-until docker ps &> /dev/null || [ $ATTEMPTS -eq 13 ]; do
-    sleep 5
-    ((ATTEMPTS++))
-done
-set -e
-
-if [ $ATTEMPTS -eq 13 ]; then
-    echo "Failed to find Docker service before timeout"
-    exit 1
-fi
 
 ARGUMENTS_ARRAY=( "$BIDS_ARGUMENTS" )
 
